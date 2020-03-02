e32c0ed5bbaa63e692f595b583a4323ea6813e44
AriixZoli <zoltan.szilagyi@ariix.com>
2020-03-02 18:04:28 +0000

    push heroku

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,21 @@
 import React from "react";
+import Logo from "./logo";
 
 export default function Footer() {
+  const logo = require('../images/logo.svg');
   return (
     <footer className="site-footer">
-      <h2> Footer </h2>
+      <Logo/>
+      <nav className="footer-nav">
+        <a href="">SOURCE</a>
+        <a href="">PRIVACY POLICY</a>
+        <a href="">DISCLAIMER</a>
+        <a href="">TERMS OF USE</a>
+        <a href="">CONTACT US</a>
+      </nav>
+      <div className="social-media">
+        Social Media Images
+      </div>
     </footer>
   );
 }
